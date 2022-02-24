7461654ad5d6cb2898bab05e5830e8ead4e1ccae
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-02-25 01:50:41 +0900

    mariaDB 연결

--- toy-board/src/main/java/com/board/toyboard/controller/BoardController.java
+++ toy-board/src/main/java/com/board/toyboard/controller/BoardController.java
@@ -1,17 +1,26 @@
 package com.board.toyboard.controller;
 
+import com.board.toyboard.model.Board;
+import com.board.toyboard.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
         return "board/list";
     }
 
