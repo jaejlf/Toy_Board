4bc3a5f5bfd9f882764cc96f9ba4f7e72d0ff662
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-08-04 19:04:28 +0900

    feat : Spring Rest Docs 발행 (#9)

--- src/test/java/com/filling/good/hello/HelloControllerTest.java
+++ src/test/java/com/filling/good/hello/HelloControllerTest.java
@@ -1,8 +1,7 @@
-package com.filling.good.common;
+package com.filling.good.hello;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.filling.good.TestConfig;
-import com.filling.good.hello.HelloController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
