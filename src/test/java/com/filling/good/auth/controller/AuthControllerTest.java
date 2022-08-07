9a2f80522fb8b78786281613355464d1d8899ed0
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-08-07 20:48:01 +0900

    test : 회원가입/로그인 컨트롤러 테스트 코드 추가 및 문서 발행 (#13)

--- src/test/java/com/filling/good/auth/controller/AuthControllerTest.java
+++ src/test/java/com/filling/good/auth/controller/AuthControllerTest.java
@@ -3,6 +3,9 @@ package com.filling.good.auth.controller;
 import com.filling.good.auth.config.JwtTokenProvider;
 import com.filling.good.auth.dto.request.LoginRequest;
 import com.filling.good.auth.dto.request.SignUpRequest;
+import com.filling.good.auth.exception.DuplicateUserException;
+import com.filling.good.auth.exception.PasswordErrorException;
+import com.filling.good.auth.exception.UserNotFoundException;
 import com.filling.good.auth.service.AuthService;
 import com.filling.good.common.CommonTest;
 import com.filling.good.user.dto.response.UserResponse;
@@ -57,7 +60,7 @@ class AuthControllerTest extends CommonTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("data").exists())
                 .andDo(print())
-                .andDo(document("auth-join",
+                .andDo(document("auth_join",
                         requestFields(
                                 fieldWithPath("email").description("이메일"),
                                 fieldWithPath("password").description("비밀번호"),
@@ -73,6 +76,44 @@ class AuthControllerTest extends CommonTest {
                                 fieldWithPath("data.fillPercent").description("Fill 퍼센테이지"),
                                 fieldWithPath("data.job").description("직업"),
                                 fieldWithPath("data.authProvider").description("가입 경로 (DEFAULT/GOOGLE)")
+                        ))
+                );
+    }
+
+    @Test
+    void 회원가입_실패_중복_회원() throws Exception {
+
+        //given
+        SignUpRequest signUpRequest = new SignUpRequest(
+                "fill@naver.com",
+                "secret1234",
+                "필링굿",
+                "학생"
+        );
+
+        given(authService.join(any())).willThrow(new DuplicateUserException());
+
+        //when
+        ResultActions actions = mockMvc.perform(post("/auth/join")
+                .content(objectMapper.writeValueAsString(signUpRequest))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("errName").value("DuplicateUserException"))
+                .andDo(print())
+                .andDo(document("auth_join_DuplicateUserException",
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호"),
+                                fieldWithPath("nickname").description("닉네임"),
+                                fieldWithPath("jobValue").description("직업 (학생/리크루터/프리랜서/개발자/기타)")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").description("상태 코드"),
+                                fieldWithPath("errName").description("예외 이름"),
+                                fieldWithPath("message").description("예외 메세지")
                         ))
                 );
     }
@@ -98,7 +139,7 @@ class AuthControllerTest extends CommonTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("data").exists())
                 .andDo(print())
-                .andDo(document("auth-login",
+                .andDo(document("auth_login",
                         requestFields(
                                 fieldWithPath("email").description("이메일"),
                                 fieldWithPath("password").description("비밀번호")
@@ -116,6 +157,74 @@ class AuthControllerTest extends CommonTest {
                         ))
                 );
 
+    }
+
+    @Test
+    void 로그인_실패_가입되지_않은_유저() throws Exception {
+
+        //given
+        LoginRequest loginRequest = new LoginRequest(
+                "fill@naver.com",
+                "secret1234"
+        );
+
+        given(authService.login(any())).willThrow(new UserNotFoundException());
+
+        //when
+        ResultActions actions = mockMvc.perform(post("/auth/login")
+                .content(objectMapper.writeValueAsString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("errName").value("UserNotFoundException"))
+                .andDo(print())
+                .andDo(document("auth_login_UserNotFoundException",
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").description("상태 코드"),
+                                fieldWithPath("errName").description("예외 이름"),
+                                fieldWithPath("message").description("예외 메세지")
+                        ))
+                );
+    }
+
+    @Test
+    void 로그인_실패_비밀번호_오류() throws Exception {
+
+        //given
+        LoginRequest loginRequest = new LoginRequest(
+                "fill@naver.com",
+                "secret1234"
+        );
+
+        given(authService.login(any())).willThrow(new PasswordErrorException());
+
+        //when
+        ResultActions actions = mockMvc.perform(post("/auth/login")
+                .content(objectMapper.writeValueAsString(loginRequest))
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errName").value("PasswordErrorException"))
+                .andDo(print())
+                .andDo(document("auth_login_PasswordErrorException",
+                        requestFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("password").description("비밀번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").description("상태 코드"),
+                                fieldWithPath("errName").description("예외 이름"),
+                                fieldWithPath("message").description("예외 메세지")
+                        ))
+                );
     }
 
     private UserResponse userResponse() {
