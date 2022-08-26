3bfb7311dcabd7721fb3fb40248abd1ab8762607
jaejeong <78673570+jaejlf@users.noreply.github.com>
2022-08-26 20:32:26 +0900

    test : User 컨트롤러 테스트 코드 작성

--- src/test/java/com/filling/good/domain/user/controller/CommonControllerTest.java
+++ src/test/java/com/filling/good/domain/user/controller/CommonControllerTest.java
@@ -1,6 +1,7 @@
 package com.filling.good.domain.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.filling.good.domain.user.entity.User;
 import com.filling.good.global.Interceptor.AuthenticationInterceptor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,11 +9,18 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static com.filling.good.domain.user.enumerate.AuthProvider.DEFAULT;
+import static com.filling.good.domain.user.enumerate.Job.STUDENT;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -27,8 +35,25 @@ public class CommonControllerTest {
     @MockBean
     public AuthenticationInterceptor authenticationInterceptor;
 
+    public User user = new User(
+            "filling@naver.com",
+            "{{ENCODED_PASSWORD}}",
+            "필링굿",
+            "필링굿",
+            STUDENT,
+            DEFAULT
+    );
+
     @BeforeEach
     public void setUp(WebApplicationContext wac, RestDocumentationContextProvider restDoc) {
+        
+        //인터셉터 통과
+        given(authenticationInterceptor.preHandle(any(), any(), any())).willReturn(true);
+        
+        //로그인 정보 세팅
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         this.mockMvc = MockMvcBuilders
                 .webAppContextSetup(wac)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
---
+++ src/test/java/com/filling/good/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,51 @@
+package com.filling.good.domain.user.controller;
+
+import com.filling.good.domain.user.service.JwtTokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+@DisplayName("User 컨트롤러")
+class UserControllerTest extends CommonControllerTest {
+
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
+
+    @DisplayName("현재 로그인된 유저 정보 조회")
+    @Test
+    void getUserInfo_200() throws Exception {
+        //given & when
+        ResultActions actions = mockMvc.perform(get("/user/info")
+                .header("X-AUTH-TOKEN", "{{ACCESS_TOKEN}}")
+                .contentType(APPLICATION_JSON));
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("user_getUserInfo",
+                        responseFields(
+                                fieldWithPath("statusCode").description("상태 코드"),
+                                fieldWithPath("message").description("결과 메세지"),
+                                fieldWithPath("data.userId").description("유저 고유 아이디"),
+                                fieldWithPath("data.email").description("이메일"),
+                                fieldWithPath("data.nickname").description("닉네임"),
+                                fieldWithPath("data.name").description("이름"),
+                                fieldWithPath("data.fillPercent").description("Fill 퍼센테이지"),
+                                fieldWithPath("data.job").description("직업"),
+                                fieldWithPath("data.authProvider").description("가입 경로 (DEFAULT/GOOGLE)")
+                        ))
+                );
+    }
+
+}
